ca2e6b3e43f636799f8a235667f77f2bcd26273b
jiefisher <jiefisher@163.com>
2025-12-08 17:18:09 +0800

    Update run.sh with new environment variables and command
    Added environment variables and a new command to run the training script.

--- examples/qwen3/run.sh
+++ examples/qwen3/run.sh
@@ -1,29 +1,33 @@
-#!/bin/bash
-set -e
-ENV=dsw
-CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-MEGATRON_PATH=$( dirname $( dirname ${CURRENT_DIR}))
-export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATH}/Megatron-LM-250328:$PYTHONPATH
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-export TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=true # for PyTorch >= 2.6
-
-if [ $ENV = dsw ]; then
-    export CUDA_VISIBLE_DEVICES=0,1
-    MASTER_ADDR=localhost
-    MASTER_PORT=$(shuf -n 1 -i 10000-65535)
-    NNODES=1
-    NODE_RANK=0
-    GPUS_PER_NODE=2
-elif [ $ENV = dlc ]; then
-    NNODES=${WORLD_SIZE}
-    NODE_RANK=${RANK}
-    GPUS_PER_NODE=${KUBERNETES_CONTAINER_RESOURCE_GPU}
-fi
-
-
-DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
-
-
-# torchrun $DISTRIBUTED_ARGS vllm_test.py
-run_cmd="torchrun $DISTRIBUTED_ARGS grpo_trainer.py"
-eval $run_cmd
+export 'PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:32'
+export 'NCCL_DEBUG=ERROR'
+export 'VLLM_CONFIGURE_LOGGING=0'
+cd /mnt/cpfs2/fangjie/cl/Pai-Megatron-Patch/examples/qwen3
+export MP_DATASET_TYPE='raw'
+sh run_mcore_qwen3.sh  \
+dsw  \
+4B   \
+1    \
+256 \
+1e-6   \
+1e-6  \
+8192  \
+8192  \
+bf16  \
+4   \
+1  \
+1 \
+1 \
+1 \
+true \
+true   \
+true \
+true \
+sel  \
+false \
+10000 \
+/mnt/cpfs2/fangjie/cl/verl/gen_entropy/sample_all.jsonl  \
+/mnt/cpfs2/fangjie/cl/verl/gen_entropy/val.jsonl  \
+/mnt/cpfs2/fangjie/Qwen3-4B-megatron \
+10000  \
+10 \
+/mnt/cpfs2/fangjie/test_save
